a94f8f0133185dee87f50bd409e936c23bf40758
Misko.Hevery <Misko.Hevery@233f3bbc-713b-0410-b3c4-5b617d06a6c5>
2008-02-15 17:07:56 +0000

    Windows file '/' and '\' clean up issues.

--- src/com/google/classpath/DirectoryClasspathRoot.java
+++ src/com/google/classpath/DirectoryClasspathRoot.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -14,8 +14,6 @@
  * the License.
  */
 package com.google.classpath;
-
-import com.google.classpath.ColonDelimitedStringParser;
 
 import java.io.File;
 import java.net.URL;
@@ -35,7 +33,7 @@ public class DirectoryClasspathRoot extends ClasspathRoot {
   @Override
   public Collection<String> getResources(String packageName) {
     List<String> resources = new LinkedList<String>();
-    File dir = new File(url.getFile() + "/" + packageName);
+    File dir = new File(url.getFile() + File.separator + packageName);
     File[] files = dir.listFiles();
     if (files == null) {
       return resources;
@@ -44,5 +42,5 @@ public class DirectoryClasspathRoot extends ClasspathRoot {
       resources.add(file.getName());
     }
     return resources;
-  } 
+  }
 }
