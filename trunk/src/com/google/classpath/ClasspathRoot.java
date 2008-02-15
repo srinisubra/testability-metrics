7362177711efb308c95198b468f7735d7d760d58
Misko.Hevery <Misko.Hevery@233f3bbc-713b-0410-b3c4-5b617d06a6c5>
2008-02-15 22:55:16 +0000

    Fix build to not include jars in jars

--- trunk/src/com/google/classpath/ClasspathRoot.java
+++ trunk/src/com/google/classpath/ClasspathRoot.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,7 +15,6 @@
  */
 package com.google.classpath;
 
-import java.io.File;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -73,12 +72,6 @@ public abstract class ClasspathRoot {
           }
           classNamesList.add(className);
         }
-      } else if (resource.endsWith(".jar")) {
-        String temp = root.getPath() + packageName + resource;
-        URL jarRoot = new File(temp).toURI().toURL();
-        ClasspathRoot jarPath = new JarClasspathRoot(jarRoot, null);
-        buildClassNamesList(jarRoot, jarPath, requiredPrefixes, "",
-            classNamesList, verbose);
       } else {
         buildClassNamesList(root, classpathRoot, requiredPrefixes, packageName
             + resource + "/", classNamesList, verbose);
